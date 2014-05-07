6d0a5332e8f40c25067ea07c4c6634dfe022ae3a
Wayne W <ww.exe@163.com>
2014-05-07 19:20:21 +0800

    add JSBridge4iOS project

---
+++ src/JSBridge4iOS/JSBridge4iOS/JSBridge4iOS.h
@@ -0,0 +1,13 @@
+//
+//  JSBridge4iOS.h
+//  JSBridge4iOS
+//
+//  Created by Wayne W on 14-5-7.
+//  
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSBridge4iOS : NSObject
+
+@end
---
+++ src/JSBridge4iOS/JSBridge4iOS/JSBridge4iOS.m
@@ -0,0 +1,13 @@
+//
+//  JSBridge4iOS.m
+//  JSBridge4iOS
+//
+//  Created by Wayne W on 14-5-7.
+//  
+//
+
+#import "JSBridge4iOS.h"
+
+@implementation JSBridge4iOS
+
+@end
